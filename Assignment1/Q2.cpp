53e22ec4f10ac39efeba3f02f178e607fb61203b
NalinCodes9 <nalinraj18@gmail.com>
2025-08-09 17:02:14 +0530

    AddedQ2

--- Assignment1/Q2.cpp
+++ Assignment1/Q2.cpp
@@ -1,1 +1,19 @@
-
+//program to remove elements which are duplicate and print unique elements in an array
+#include<iostream> 
+using namespace std; 
+int main(){ 
+    int arr[]={2,3,3,4,4,5}; 
+    int size=sizeof(arr)/sizeof(arr[0]); 
+    int arr1[size];//we are create a new array to store unique elements 
+    arr1[0]=arr[0]; 
+    int x=0; 
+    for(int i=1 ; i<size ; i++){ 
+        if(arr1[x]!=arr[i]){ 
+            x++; 
+            arr1[x]=arr[i]; 
+        } 
+    } 
+    for(int i = 0 ; i<=x ; i++){ 
+        cout<<arr1[i]<<" "<<endl;
+    }
+}
