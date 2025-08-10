38f32f7902dbb8b77a0852a1bc540cfbe0888a44
NalinCodes9 <nalinraj18@gmail.com>
2025-08-10 12:27:10 +0530

    AddedQ4b

---
+++ Assignment2/Q4b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string stri;
+    cout<<"Enter a string "<<endl;
+    getline(cin,stri);
+    int low=0;
+    int high=stri.length()-1;
+    while(low<high){
+        int t=stri[high];
+        stri[high]=stri[low];
+        stri[low]=t;
+        low++;
+        high--;
+    }
+    cout<<"Reversed String : "<<stri;
+}
