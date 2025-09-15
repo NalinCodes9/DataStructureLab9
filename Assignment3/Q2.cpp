0d33a8a1c6113f16b7e527657a0b90a55eb978a7
NalinCodes9 <nalinraj18@gmail.com>
2025-09-16 03:28:12 +0530

    Q2.cpp

--- Assignment3/Q2.cpp
+++ Assignment3/Q2.cpp
@@ -1,50 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-
-#define MAX 100   // maximum size of stack
-
-char stack[MAX];
-int top = -1;
-
-// push function
-void push(char c) {
-    if (top == MAX - 1) {
-        printf("Stack Overflow! Cannot push %c\n", c);
-    } else {
-        stack[++top] = c;
+#include <iostream>
+#include <stack>
+using namespace std;
+string revString(string s){
+    stack<char>st;
+    for(int i=0;i<s.length();i++){
+        st.push(s[i]);
     }
+    string Rev = "";
+    while(!st.empty()){
+        Rev += st.top();
+        st.pop();
+    }
+    return Rev;
 }
-
-// pop function
-char pop() {
-    if (top == -1) {
-        printf("Stack Underflow!\n");
-        return '\0';
-    } else {
-        return stack[top--];
-    }
-}
-
-int main() {
-    char str[100];
-    int i;
-
-    printf("Enter a string: ");
-    scanf("%s", str);   // taking input (single word, no spaces)
-
-    int len = strlen(str);
-
-    // push all characters into stack
-    for (i = 0; i < len; i++) {
-        push(str[i]);
-    }
-
-    // pop all characters to reverse
-    printf("Reversed string is: ");
-    for (i = 0; i < len; i++) {
-        printf("%c", pop());
-    }
-    printf("\n");
-
-    return 0;
+int main(){
+    string s;
+    cout<<"ENTER THE STRING ->";
+    cin>>s;
+    cout<<revString(s);
 }
